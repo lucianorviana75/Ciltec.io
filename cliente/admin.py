c3f93694efb220a71002c3dc2c590dea9e5d7190
luciano <ribeirovianaluciano98@gmail.com>
2024-09-26 23:41:01 -0300

    basico do django

--- cliente/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from.models import Cliente
-from.models import Servicos
-from.models import Area
-
-admin.site.register(Cliente)
-admin.site.register(Servicos)
-admin.site.register(Area)
